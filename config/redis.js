a428f7040fde18dc70b413519a74a86b1e5b9bf8
vijju <puneetsingh10091998@gmail.com>
2026-01-07 10:31:01 +0000

    fixed bugs

--- config/redis.js
+++ config/redis.js
@@ -3,6 +3,12 @@ const Redis = require("ioredis");
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
+   maxRetriesPerRequest: null,
+  enableReadyCheck: true,
+    retryStrategy(times) {
+    return Math.min(times * 100, 2000);
+  },
+
 });
 
 redis.on("connect", () => {
